965c43f8813108ab1b683a1e929768be6355cb36
Thiratt <thiratcha7@gmail.com>
2026-08-18 19:26:43 +0700

    refactor: migrate to Next.js

---
+++ src/app/page.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import {
+	BudgetComparisonChart,
+	DroughtFoodMenu,
+	DroughtRecovery,
+	DroughtTraditions,
+	HeroSection,
+	IsanRiverBasins,
+	LangCircleSection,
+	MyLangIsNotEqual,
+	NavigationBar,
+	NewGenFooter,
+	TemperatureHistory,
+	ThaiRiverBasinsMap,
+	WhatIsLangSection,
+} from "@/components";
+import { useActiveSection, useSectionNavigation, useSectionRefs } from "@/hooks";
+
+export default function RootPage() {
+	const {
+		sectionRefs,
+		heroSectionRef,
+		whatIsLangSectionRef,
+		langCircleSectionRef,
+		temperatureHistoryRef,
+		myLangIsNotEqualRef,
+		thaiRiverBasinsMapRef,
+		isanRiverBasinsRef,
+		droughtTraditionsRef,
+		droughtFoodMenuRef,
+		budgetComparisonChartRef,
+		droughtRecoveryRef,
+	} = useSectionRefs();
+
+	const activeSection = useActiveSection(sectionRefs);
+	const { scrollToSection } = useSectionNavigation(sectionRefs);
+
+	const scrollToWhatIsLangSection = () => {
+		whatIsLangSectionRef.current?.scrollIntoView({
+			behavior: "smooth",
+			block: "start",
+		});
+	};
+
+	return (
+		<>
+			<NavigationBar activeSection={activeSection} onNavigate={scrollToSection} />
+			<HeroSection ref={heroSectionRef} nextPage={scrollToWhatIsLangSection} />
+			<WhatIsLangSection ref={whatIsLangSectionRef} />
+			<LangCircleSection ref={langCircleSectionRef} />
+			<TemperatureHistory ref={temperatureHistoryRef} />
+			<MyLangIsNotEqual ref={myLangIsNotEqualRef} />
+			<BudgetComparisonChart ref={budgetComparisonChartRef} />
+			{/* <HeatImpactChart ref={heatImpactChartRef} /> */}
+			<ThaiRiverBasinsMap ref={thaiRiverBasinsMapRef} />
+			<IsanRiverBasins ref={isanRiverBasinsRef} />
+			<DroughtTraditions ref={droughtTraditionsRef} />
+			<DroughtFoodMenu ref={droughtFoodMenuRef} />
+			<DroughtRecovery ref={droughtRecoveryRef} />
+			<NewGenFooter ref={null} />
+		</>
+	);
+}
